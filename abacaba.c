3b2437fca25bd6400e41884e354ed64d95a77e56
artemiy <username@debian>
2018-09-19 16:30:21 +0300

    deadline 20.09

---
+++ abacaba.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+void GenerateString(int n,char* string){
+	int i;
+	char k='a';
+	char new_string[1<<n];
+	string[0]='a';
+	printf("a\n");
+	for (i=1;i<n;i++){
+		k+=1;
+		strcpy(new_string,string);
+		string[strlen(string)]=k;
+		strcat(string,new_string);
+		printf("%s\n",string);
+	}
+}
+
+
+int main(){
+	char* string;
+	int n;
+	scanf("%d",&n);
+	string=malloc(sizeof(*string)*((1<<n)-1));
+	GenerateString(n,string);
+	free(string);
+	return 0;
+}
