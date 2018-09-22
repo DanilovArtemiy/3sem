c2fb43451b94dd7188205933ffa636d9bbb8e92c
Vlad <vlad.miryaha@gmail.com>
2018-09-22 21:45:55 +0300

    fixit

--- split.c
+++ split.c
@@ -7,6 +7,7 @@ void Split(char* string,char* delimiters,char*** tokens,int* tokensCount){
 	int k=0, add=0, start=0;
 	int len = strlen(string);
 	for (k=0;k<=len;k++){
+		// FIXIT: так не пойдет, вы предполагаете, что разделителей ровно 2. посмотрите еще раз на strtok и используйте её
 		if (string[k] == delimiters[0] || string[k] == delimiters[1]){
 			string[k] = '\0';
 			(*tokens)[(*tokensCount)++] = strdup(&string[start]);
@@ -20,6 +21,9 @@ void Split(char* string,char* delimiters,char*** tokens,int* tokensCount){
 }	
 int main(){
 	int i=0,j=0;
+	// FIXIT: 1) не должно быть магических констант в коде. Напишите #define MAX_STRING_SIZE 100 и используйте
+	// Причем где-то у вас 100 означает MAX_TOKENS_COUNT 
+	// 2) задание было чуть надумано и нужно было вынести выделение и освобождение памяти в отдельные ф-и. поправьте.
 	char string[100] = {};
 	int tokensCount=0;
 	char delimiters[2]={'\t','\n'};
