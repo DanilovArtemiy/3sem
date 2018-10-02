ff47d494f7becc22075dc5dd700c3bfa33b15ae2
artemiy <username@debian>
2018-10-02 19:16:20 +0300

    deadline 20.09

--- split.c
+++ split.c
@@ -1,41 +1,39 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#define MAX_STRING_SIZE 100
+#define MAX_TOKENS_COUNT 100
+#define MAX_DELIMITERS_SIZE 10
 
 
-void Split(char* string,char* delimiters,char*** tokens,int* tokensCount){
-	int k=0, add=0, start=0;
-	int len = strlen(string);
-	for (k=0;k<=len;k++){
-		if (string[k] == delimiters[0] || string[k] == delimiters[1]){
-			string[k] = '\0';
-			(*tokens)[(*tokensCount)++] = strdup(&string[start]);
-			add++;
-			start = add;
-		}
-		else{
-		add++;
-		}
+void Split(char* string, char* delimiters, char*** tokens, int* tokensCount){
+	int i=0;
+	(*tokens[i])=strtok(string,delimiters);
+	while ((*tokens)[i]!=NULL){
+		i++;
+		(*tokens)[i]=strtok(NULL,delimiters);
 	}
-}	
+	*tokensCount=i;
+}
+
+
+
 int main(){
 	int i=0,j=0;
-	char string[100] = {};
+	char string[MAX_STRING_SIZE];
 	int tokensCount=0;
-	char delimiters[2]={'\t','\n'};
-	fgets(string,100,stdin);
+	char delimiters[MAX_DELIMITERS_SIZE];
+	fgets(string,MAX_STRING_SIZE,stdin);
+	fgets(delimiters,MAX_DELIMITERS_SIZE,stdin);
 	char **tokens;
-	tokens=(char**)malloc(100*sizeof(char*));
-	for (i=0;i<100;i++){
-		tokens[i]=(char*)malloc(100*sizeof(char));}
+	tokens=(char**)malloc(MAX_TOKENS_COUNT*sizeof(char*));
+	for (i=0;i<MAX_TOKENS_COUNT;i++){
+		tokens[i]=(char*)malloc(MAX_STRING_SIZE*sizeof(char));}
 
 	Split(string,delimiters,&tokens,&tokensCount);
 
 	for (i=0;i<tokensCount;i++){
 		printf("%s\n",tokens[i]);
-	}
-	for (i=0;i<100;i++){
-		free(tokens[i]);
 	}
 	free(tokens);
 	return 0;
