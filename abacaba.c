7a192c162d81491ef39adf6c8b960ef3c8ef91c4
artemiy <username@debian>
2018-10-09 11:26:05 +0300

    deadline 20.09

--- abacaba.c
+++ abacaba.c
@@ -3,18 +3,18 @@
 #include<stdlib.h>
 // после бинарных операторов и запятых ставье пробел: char k = 'a'; strcat(s, ns); и т.п.
 
-void GenerateString(int n,char* string){
+void GenerateString(int n, char* string){
 	int i;
-	char k='a';
-	char new_string[1<<n];
-	string[0]='a';
+	char k = 'a';
+	char new_string[1 << n];
+	string[0] = 'a';
 	printf("a\n");
-	for (i=1;i<n;i++){
-		k+=1;
-		strcpy(new_string,string);
-		string[strlen(string)]=k;
-		strcat(string,new_string);
-		printf("%s\n",string);
+	for (i = 1; i < n; i++){
+		k += 1;
+		strcpy(new_string, string);
+		string[strlen(string)] = k;
+		strcat(string, new_string);
+		printf("%s\n", string);
 	}
 }
 
@@ -22,9 +22,9 @@ void GenerateString(int n,char* string){
 int main(){
 	char* string;
 	int n;
-	scanf("%d",&n);
-	string=malloc(sizeof(*string)*((1<<n)-1));
-	GenerateString(n,string);
+	scanf("%d", &n);
+	string = malloc(sizeof(*string) * ((1 << n) - 1));
+	GenerateString(n, string);
 	free(string);
 	return 0;
 }
