e8332f445ff3e94cd8c683de028fec3982f82111
artemiy <username@debian>
2018-10-13 20:35:41 +0300

    deadline 04.10

--- commands_list.c
+++ commands_list.c
@@ -6,6 +6,8 @@
 
 #define DEAD_TIME 10
 #define MAX_COMMAND_COUNT 100
+#define MAX_STRING_LENGTH 100
+#define filename "text.txt"
 
 void Split (char* string, char* delimiters, char*** tokens, int* tokensCount){
 	int i = 0;
@@ -26,32 +28,15 @@ int main(){
 	FILE* myfile;
 	char delimiters_for_string[1] = {'\n'};
 	char delimiters[3] = {'\t', '\n', ' '};
-	
-	// FIXIT: отдельная константа для 100
-	char str[100] = {};
-	myfile = fopen("text.txt", "r");
+	char str[MAX_STRING_LENGTH] = {};
+	myfile = fopen(filename, "r");
 	int i = 0, j = 0, len = 0;
 	pid_t pid = fork();
-	while(1){
-		// Видимо стоит переименовать MAX_COMMAND_COUNT в MAX_COMMAND_LENGTH
-		if (fgets(str, MAX_COMMAND_COUNT, myfile) != NULL){
-			strcat(action, str);
-			time[i] = atoi(str);
-			i++;
-		}
-		else {
-			break;
-		}
+	while (fgets(str, MAX_STRING_LENGTH, myfile)){ 
+		strcat(action, str);
+		time[i] = atoi(str);
+		i++;
 	}
-	
-	/* Цикл выше можно напистать короче: 
-		while (fgets(str, MAX_COMMAND_COUNT, myfile)) 
-		{
-			strcat(action, str);
-			time[i] = atoi(str);
-			i++;
-		}
-	*/
 	char **tokens;
 	tokens = (char**)malloc(MAX_COMMAND_COUNT * sizeof(char*));
 	for (i = 0; i < MAX_COMMAND_COUNT; i++){
@@ -67,28 +52,19 @@ int main(){
 			arg[f] = (char*)malloc(MAX_COMMAND_COUNT * sizeof(char));
 		}
 		Split(tokens[i], delimiters, &arg, &j);
-		// Кажется можно просто запускать команды вот так execvp(arg[1], arg + 1); вместо сдвигания массива на 1 элемент
-		int k = 0;
-		for (k = 0; k < MAX_COMMAND_COUNT - 1; k++){
-			arg[k] = arg[k + 1];
-		}
 		pid = fork();
 		if (pid == 0){
 			pid = fork();
 			if (pid == 0){
 				sleep(time[i]);
-				execvp(arg[0], arg);
-				// вот здесь лучше написать exit(0) на всякий случай. если команду запустить не получится, то и исходный родительский и этот дочерний процесс продолжат запускать новые поцессы, чего явно не хочется
+				execvp(arg[1], arg+1);
 			} else {
 				sleep(time[i] + DEAD_TIME);
-				if (waitpid(pid, &status, WNOHANG)){
-					exit(-1);
-				} else {
+				if (!waitpid(pid, &status, WNOHANG)){
 					kill(pid, SIGKILL);
-					exit(-1);
 				}
-				// лучше exit(-1) написать здесь, т.к. он все равно в обоих ветках if вызывается
 			}
+			exit(-1);
 		} 
 		free(arg);
 	}
